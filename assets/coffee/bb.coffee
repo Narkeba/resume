d31cdd8f1582861429b6236b3be881752c288350
m.balkhaev <m.balkhaev@oversun.ru>
2014-01-15 17:44:22 +0400

    Add fix top menu && scrollspy && delete docs

--- assets/coffee/bb.coffee
+++ assets/coffee/bb.coffee
@@ -1,2 +1,7 @@
 $(document).ready ->
 	$('#additionally').collapse()
+	$('body').scrollspy target: 'nav', offset: 80
+	$('.navbar li a').click (event) ->
+		event.preventDefault()
+		$($(this).attr 'href')[0].scrollIntoView()
+		scrollBy 0, -79
--- build/js/all.js
+++ build/js/all.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  return $('#additionally').collapse();
+  $('#additionally').collapse();
+  $('body').scrollspy({
+    target: 'nav',
+    offset: 80
+  });
+  return $('.navbar li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    return scrollBy(0, -79);
+  });
 });
